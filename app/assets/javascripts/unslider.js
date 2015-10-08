7104bdf2e4b0125f7244c1ac49f55f6983921d33
codecademy <ccuser@codecademy.com>
2015-10-07 21:28:12 -0400

    added unslider stuff, got it to work

---
+++ app/assets/javascripts/unslider.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.banner').unslider({
+     	speed: 500, 
+      delay: 1000,
+      keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+			dots: true              //  Display dot navigation
+    });
+  
+});
